642ffdaab8e9e1b08b113be0bf5134bb9335a0ac
Filip Navara <navara@emclient.com>
2018-07-18 17:37:01 +0200

    Address review comments.

--- src/System.IO.Pipes/src/System/IO/Pipes/NamedPipeClientStream.Windows.cs
+++ src/System.IO.Pipes/src/System/IO/Pipes/NamedPipeClientStream.Windows.cs
@@ -16,10 +16,6 @@ namespace System.IO.Pipes
     /// </summary>
     public sealed partial class NamedPipeClientStream : PipeStream
     {
-#if MONO
-        int _access = 0;
-#endif
-
         // Waits for a pipe instance to become available. This method may return before WaitForConnection is called
         // on the server end, but WaitForConnection will not return until we have returned.  Any data written to the
         // pipe by us after we have connected but before the server has called WaitForConnection will be available
