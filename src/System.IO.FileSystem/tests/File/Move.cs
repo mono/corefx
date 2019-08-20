c2cd77ebdf096c4af04ae243113d342fa2a379c8
Alexander Köplinger <alex.koeplinger@outlook.com>
2019-08-20 20:17:00 +0200

    Update Move.cs

--- src/System.IO.FileSystem/tests/File/Move.cs
+++ src/System.IO.FileSystem/tests/File/Move.cs
@@ -186,7 +186,7 @@ namespace System.IO.Tests
             string missing_target = GetTestFilePath();
             string dangling_symlink_new_location = GetTestFilePath();
             Assert.False(File.Exists(missing_target));
-             Assert.Equal(0, symlink(missing_target, dangling_symlink));
+            Assert.Equal(0, symlink(missing_target, dangling_symlink));
             Move(dangling_symlink, dangling_symlink_new_location);
             Assert.True(File.Exists(dangling_symlink_new_location)); // File.Exists returns true for dangling symlinks
         }
